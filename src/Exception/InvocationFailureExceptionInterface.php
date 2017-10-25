de58f069433732169daceab3b7fded408740ff2a
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-25 18:14:44 +0200

    Added `InvocationFailureExceptionInterface`

---
+++ src/Exception/InvocationFailureExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Invocation\Exception;
+
+/**
+ * An exception that occurs when an invoker fails to invoke a command.
+ *
+ * @since [*next-version*]
+ */
+interface InvocationFailureExceptionInterface extends CommandInvokerExceptionInterface
+{
+}
