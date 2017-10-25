b0dabe51521fb4c3f0aa948fd6a810f83d32b177
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-25 16:03:18 +0200

    Added `CommandInvokerInterface`
    Renamed from `CodeInvokerInterface`

--- src/CommandInvokerInterface.php
+++ src/CommandInvokerInterface.php
@@ -5,18 +5,18 @@ namespace Dhii\Invocation;
 use Dhii\Util\String\StringableInterface as Stringable;
 
 /**
- * Something that can invoke commands by code.
+ * Something that can invoke commands.
  *
  * @since [*next-version*]
  */
-interface CodeInvokerInterface
+interface CommandInvokerInterface
 {
     /**
-     * Invokes a command by code.
+     * Invokes a command.
      *
      * @since [*next-version*]
      *
-     * @param string|Stringable $code   The code of the command to invoke.
+     * @param string|Stringable $code   The command to invoke.
      * @param array             $params The parameters to pass to the command.
      */
     public function invoke($code, array $params = []);
