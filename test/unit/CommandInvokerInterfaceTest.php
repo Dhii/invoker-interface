9b4bde271f19d79756647c6e0849cfcf3224cf7a
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-25 16:02:46 +0200

    Added test for `CommandInvokerInterface`

---
+++ test/unit/CommandInvokerInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dhii\Io\Resource\UnitTest;
+
+use Xpmock\TestCase;
+use Dhii\Invocation\CommandInvokerInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class CommandInvokerInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Invocation\CommandInvokerInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                ->invoke()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created');
+    }
+}
