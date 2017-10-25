d05b62b53965a62ae5d6c6eccb3c3d59c0a1c8c2
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-25 18:14:29 +0200

    Added test for `InvocationFailureExceptionInterface`

---
+++ test/unit/Exception/InvocationFailureExceptionInterfaceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dhii\Invocation\UnitTest\Exception;
+
+use Xpmock\TestCase;
+use Dhii\Invocation\InvocationFailureExceptionInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class InvocationFailureExceptionInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Invocation\Exception\InvocationFailureExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                ->getFile()
+                ->getLine()
+                ->getMessage()
+                ->getCode()
+                ->getPrevious()
+                ->getTrace()
+                ->getTraceAsString()
+                ->__toString()
+
+                ->getCommandInvoker()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created');
+        $this->assertInstanceOf('Dhii\Invocation\CommandInvokerAwareInterface', $subject, 'A valid instance of the subject could not be created');
+    }
+}
