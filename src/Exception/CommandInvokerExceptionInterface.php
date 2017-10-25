64d5e973c7c7f5625589ba3a26e08747bdda0f2d
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-25 16:39:01 +0200

    Added `CommandInvokerExceptionInterface`

---
+++ src/Exception/CommandInvokerExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Invocation\Exception;
+
+use Dhii\Invocation\CommandInvokerAwareInterface;
+
+/**
+ * An exception related to a command invoker.
+ *
+ * @since [*next-version*]
+ */
+interface CommandInvokerExceptionInterface extends CommandInvokerAwareInterface
+{
+}
