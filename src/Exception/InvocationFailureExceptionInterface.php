d0928e97449059e39dafbf488b0a81393052c941
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-26 15:41:28 +0200

    `InvocationFailureExceptionInterface` is now aware of the command and the args

--- src/Exception/InvocationFailureExceptionInterface.php
+++ src/Exception/InvocationFailureExceptionInterface.php
@@ -2,11 +2,17 @@
 
 namespace Dhii\Invocation\Exception;
 
+use Dhii\Invocation\CommandAwareInterface;
+use Dhii\Invocation\ArgsAwareInterface;
+
 /**
  * An exception that occurs when an invoker fails to invoke a command.
  *
  * @since [*next-version*]
  */
-interface InvocationFailureExceptionInterface extends CommandInvokerExceptionInterface
+interface InvocationFailureExceptionInterface extends
+        CommandInvokerExceptionInterface,
+        CommandAwareInterface,
+        ArgsAwareInterface
 {
 }
