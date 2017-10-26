8a2ba6de4b509b285b28e1d08db5e5aabc304408
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-26 15:37:42 +0200

    Addede test for `ArgsAwareInterface`

---
+++ test/unit/ArgsAwareInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dhii\Invocation\UnitTest;
+
+use Xpmock\TestCase;
+use Dhii\Invocation\ArgsAwareInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class ArgsAwareInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Invocation\ArgsAwareInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                ->getArgs()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created');
+    }
+}
