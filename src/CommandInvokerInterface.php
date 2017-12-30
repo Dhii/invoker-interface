7c8814ff1cc3a7de91d873e4b8ed2b3a8daadea7
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-30 16:11:45 +0100

    Invoker now declares that it can throw exceptions

--- src/CommandInvokerInterface.php
+++ src/CommandInvokerInterface.php
@@ -2,8 +2,11 @@
 
 namespace Dhii\Invocation;
 
+use Dhii\Invocation\Exception\CommandInvokerExceptionInterface;
+use Dhii\Invocation\Exception\InvocationFailureExceptionInterface;
 use Dhii\Util\String\StringableInterface as Stringable;
 use Traversable;
+use Exception as RootException;
 
 /**
  * Something that can invoke commands.
@@ -19,6 +22,10 @@ interface CommandInvokerInterface
      *
      * @param string|Stringable $code The command to invoke.
      * @param array|Traversable $args The arguments to pass to the command, if any
+     *
+     * @throws InvocationFailureExceptionInterface If the command could not be invoked.
+     * @throws CommandInvokerExceptionInterface    For any problem related to the invoker.
+     * @throws RootException                       For any problem not related to the invoker.
      */
     public function invoke($code, $args = null);
 }
